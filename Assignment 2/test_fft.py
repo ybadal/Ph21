98f36256f98074eff6c9f3feca11ed7e1264feb4
Yovan Badal <yovanbadal@gmail.com>
2018-04-20 13:42:49 -0700

    cos fft

--- Assignment 2/test_fft.py
+++ Assignment 2/test_fft.py
@@ -1,15 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+Fs = 150.0;  # sampling rate
+Ts = 1.0/Fs; # sampling interval set to L=1
+t = np.arange(0,1,Ts) # time vector
 
-N = 200
-T = 1.0/200
-x = np.linspace(0.0, N*T, N)
-y = 2*np.cos((2.0*np.pi)*x/200.0) + 3
-yf = np.fft.fft(y)
-xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+y = 3*np.cos(2*np.pi*4*t + 0.4) + 1 # frequency set to 4/L=4
 
+n = len(y) # length of the signal
+k = np.arange(n)
+T = n/Fs
+frq = k/T # two sides frequency range
+frq = frq[range(n/2)] # one side frequency range
 
-fig, ax = plt.subplots()
-ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
-plt.show()
+Y = np.fft.fft(y)/n # fft computing and normalization
+Y_disp = Y[range(n/2)]
+
+inv = np.fft.ifft(Y)
+
+fig, ax = plt.subplots(3, 1)
+ax[0].plot(t,y)
+ax[0].set_xlabel('Time')
+ax[0].set_ylabel('Amplitude')
+ax[0].set_title('Signal')
+ax[1].plot(frq,abs(Y_disp),'r') # plotting the spectrum
+ax[1].set_xlabel('Freq (Hz)')
+ax[1].set_ylabel('|Y(freq)|')
+ax[1].set_title('Positive Spectrum (FFT)')
+ax[2].plot(t,inv)
+ax[2].set_xlabel('Time')
+ax[2].set_ylabel('Amplitude')
+ax[2].set_title('Recovered Signal (Inverse FFT)')
+plt.tight_layout()
+plt.savefig('cosine_signal.png')
